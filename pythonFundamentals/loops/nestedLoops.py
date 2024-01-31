361a4e875279b373fa10778a5ac7e1597c989205
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2024-01-31 11:47:12 +0545

    31st Jan new files

---
+++ pythonFundamentals/loops/nestedLoops.py
@@ -0,0 +1,12 @@
+# for i in range(1,6):
+#     for j in range(0,i):
+#         print("*", end="")
+#     print("\n")
+#
+#
+#
+
+for i in range(1,6):
+    for j in range(0,6-i):
+        print("*", end="")
+    print("\n")
