8050be9796100075869740c8a87a7d137e419078
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2024-02-04 11:09:07 +0545

    star print using while Loop

---
+++ pythonFundamentals/loops/starWhileLoop.py
@@ -0,0 +1,26 @@
+# for i in range(1, 6):
+#     for j in range(0, i):
+#         print("*", end='')
+#     print("\n")
+
+
+# i = 1
+# n = 5
+# while i <= n:
+#     j = 1
+#     while j <= i:
+#         print("*", end='')
+#         j += 1  # j = j + 1
+#     print("\n")
+#     i += 1  # i = i + 1
+
+
+i = 1
+n = 5
+while i <= n:
+    j = 0
+    while j <= n - i:
+        print("*", end='')
+        j += 1  # j = j + 1
+    print("\n")
+    i += 1  # i = i + 1
--- pythonFundamentals/practice.py
+++ pythonFundamentals/practice.py
@@ -1,34 +1,3 @@
-# for i in range (9,0,-1):
-#     print(i)
-
-# simple add function
-
-
-# number1 = int(input("Enter number1: "))
-# number2 = int(input("Enter number1: "))
-#
-#
-# def add_things(n1, n2):
-#     add = n1 + n2
-#     sub = n1 - n2
-#
-#     return add, sub
-#
-#
-# addition, subtraction = add_things(number1, number2)
-#
-# print("Addition:", addition)
-# print("Subtraction:", subtraction)
-
-
-# star
-
-# for i in range(0, 3):
-#     for j in range(0, 3):
-#         print("*", end="")
-#     print("\n")
-
-
 number1 = int(input("Number1: "))
 number2 = int(input("Number2: "))
 
