774a423485d4fa715c36aeb78773264c54379321
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2024-02-07 10:53:29 +0545

    printed star using function

--- pythonFundamentals/loops/starUsingFunction.py
+++ pythonFundamentals/loops/starUsingFunction.py
@@ -1,6 +1,16 @@
+# def pattern_generator(n, pattern):
+#     for i in range(1, n + 1):
+#         for j in range(0, i):
+#             print(pattern, end='')
+#         print("\n")
+#
+#
+# pattern_generator(5, "%")
+
+
 def pattern_generator(n, pattern):
-    for i in range(1, n + 1):
-        for j in range(0, i):
+    for i in range(n):
+        for j in range(0, n-i):
             print(pattern, end='')
         print("\n")
 
