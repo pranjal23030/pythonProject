fddeadeb921396115e4530b933bf25d98343f864
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2024-02-23 12:56:49 +0545

    further code added

---
+++ pythonFundamentals/list/2D_List.py
@@ -0,0 +1,6 @@
+persons = ["John", "Marissa", "Peta"]
+restaurants = ["Japanese", "Mexican", "Thai"]
+
+for person in persons:
+    for restaurant in restaurants:
+        print(person, restaurant)
---
+++ pythonFundamentals/list/empty_List_Practice.py
@@ -0,0 +1,9 @@
+container = []
+num = 1
+
+while num < 20:
+    if num > 10:
+        container.append(num)
+    num += 1
+
+print(container)
