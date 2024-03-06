eeb69a73146158e3889bb72f641dc5e3b4127bdc
Pranjal Khatiwada <pranjal.khatiwoda@gmail.com>
2024-03-06 21:51:27 +0545

    started a django demo project

---
+++ pythonFundamentals/OOP/allen_card.py
@@ -0,0 +1,120 @@
+import random
+
+
+class Card(object):
+    """Represents a standard playing card.
+    
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", 
+              "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a human-readable string representation."""
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+
+    def __lt__(self, other):
+        """Compares this card to other, first by suit, then rank.
+
+        Returns a positive number if this > other; negative if other > this;
+        and 0 if they are equivalent.
+        """
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1>t2
+
+
+class Deck(object):
+    """Represents a deck of cards.
+
+    Attributes:
+      cards: list of Card objects.
+    """
+    
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def add_card(self, card):
+        """Adds a card to the deck."""
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        """Removes a card from the deck."""
+        self.cards.remove(card)
+
+    def pop_card(self, i=-1):
+        """Removes and returns a card from the deck.
+
+        i: index of the card to pop; by default, pops the last card.
+        """
+        return self.cards.pop(i)
+
+    def shuffle(self):
+        """Shuffles the cards in this deck."""
+        random.shuffle(self.cards)
+
+    def sort(self):
+        """Sorts the cards in ascending order."""
+        self.cards.sort()
+
+    def move_cards(self, hand, num):
+        """Moves the given number of cards from the deck into the Hand.
+
+        hand: destination Hand object
+        num: integer number of cards to move
+        """
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+    
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    deck.shuffle()
+
+    pradeep = Hand()
+    nipesh = Hand()
+    aashik = Hand()
+    
+    deck.move_cards(pradeep, 3)
+    deck.move_cards(nipesh, 3)
+    deck.move_cards(aashik, 3)
+    
+    pradeep.sort()
+    nipesh.sort()
+    aashik.sort()
+
+    print("Pradeep Hand")
+    print(pradeep)
+    print("\nNipesh Hand")
+    print(nipesh)
+    print("\nAAshik Hand")
+    print(aashik)
+    
---
+++ pythonFundamentals/OOP/card_v2.py
@@ -0,0 +1,55 @@
+import random
+
+class Card:
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8",
+             "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        #return f"{self.rank} of {self.suit}"
+        return f"{Card.ranks[self.rank]} of {Card.suits[self.suit]}"
+
+
+
+
+
+
+
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.update()
+        #self.shuffle()
+
+    def update(self):
+##        for suit in Card.suits:
+##            for rank in Card.ranks:
+##                self.cards.append(Card(suit, rank))
+        for suit in range(4):
+            for rank in range(13):
+                self.cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        output = ""
+        for card in self.cards:
+            output += card.__str__() + "\n"
+
+        return output
+
+d = Deck()
+print(d)
+    
+##c = Card()
+##print(c)
+
+
+
